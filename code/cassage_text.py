d96ed8f404df4170e9b4d544540ef032fc3235a2
ahmet.baba <ahmet.baba@etu.univ-orleans.fr>
2023-12-11 11:29:02 +0100

    exploit : ajout de la methode du code astucieux

--- code/cassage_text.py
+++ code/cassage_text.py
@@ -1,4 +1,4 @@
-from SDES import double_encod
+from SDES import double_encod,decod_text,encod_text
 from CONSTANTE import MAX_BYTE_SDES
 def cassage_brutale(message_clair:str,message_chiffre:list) -> tuple:
     """
@@ -22,3 +22,30 @@ def cassage_brutale(message_clair:str,message_chiffre:list) -> tuple:
                 return (cle1,cle2)
     print("les clé n'ont pas était trouver !!")
 
+def cassage_astucieux(message_clair:str,message_chiffre:list) -> tuple:
+    """
+        Cette methode va nous permettre de trouver les deux cle qui ont permit de passer
+        du message claire au message crypte en utilisant une methode astucieuse et plus rapide
+        Args:
+            param:
+                message_clair(string) : le message claire
+                message_chiffre(list): une liste d'entier representant les lettre du message chiffre
+            return:
+                couple (tuple) : la clé 1 et la clé 2 
+    """
+    cle1=[]
+    cle2=[]
+    c1=-1
+    c2=-1
+    for k1 in range(MAX_BYTE_SDES):
+        cle1.append(encod_text(message_clair,k1))
+        cle2.append(decod_text(message_chiffre,k1))
+    intersection = list(filter(lambda x: x in cle1, cle2))
+    for i in range(len(cle1)):
+        if cle1[i] == intersection[0]:
+            c1=i
+        if cle2[i] == intersection[0]:
+            c2=i
+    if c1!=-1 and c2!=-1:
+        return (c1,c2)
+    return "cassage impossible"
--- tests/test_cassage_brutale.py
+++ tests/test_cassage_brutale.py
@@ -14,11 +14,17 @@ sys.path.append(os.path.join(ROOT,'doc/'))
 class TestCassageFunctions(unittest.TestCase):
 
     def test_cassage_brutale(self):
+        """
+            Cette methode va nous permettre de tester le cassage brutale sur un message simple
+        """
         texte_clair = "Hello"
         texte_chiffre = double_encod(texte_clair, 10,10)
         self.assertEqual(cassage_brutale(texte_clair,texte_chiffre), (10,10))
 
     def test_arsene_lupin(self):
+        """
+            Cette methode va nous permettre de tester le cassage brutale sur un extrait du livre arsene lupin
+        """
         fichier_texte = os.path.join(ROOT, 'doc/arsene_lupin_extrait.txt')
         with open(fichier_texte, 'r', encoding='utf-8') as file:
             texte_clair=file.read()
