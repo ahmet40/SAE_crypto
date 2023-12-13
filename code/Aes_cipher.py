1c54c79e6db9f97f0483ed0517fd8418909e62ae
ahmet.baba <ahmet.baba@etu.univ-orleans.fr>
2023-12-13 10:59:00 +0100

    exploit : ajout du fichier permettant de retrouver la cle de chiffrrement

--- code/Aes_cipher.py
+++ code/Aes_cipher.py
@@ -4,14 +4,23 @@ from Crypto.Util.Padding import pad, unpad
 
 
 def generate_aes_key():
+    """
+        Generates a random 256-bit AES key.
+    """
     return get_random_bytes(32)  # 32 bytes = 256 bits
 
 def aes_encrypt(message, key):
+    """
+        Encrypts the given message using the given key.
+    """
     cipher = AES.new(key, AES.MODE_CBC)
     ciphertext = cipher.encrypt(pad(message.encode('utf-8'), AES.block_size))
     return ciphertext, cipher.iv
 
 def aes_decrypt(ciphertext, key, iv):
+    """
+        Decrypts the given ciphertext using the given key and iv.
+    """
     cipher = AES.new(key, AES.MODE_CBC, iv)
     decrypted_message = unpad(cipher.decrypt(ciphertext), AES.block_size)
     return decrypted_message.decode('utf-8')
